25ee57736192af28959caf9ca62a404a4a9dd760
Krasimir Tsonev <krasimir@outset.ws>
2013-01-09 00:48:13 +0200

    Adding login/logout logic

--- index.php
+++ index.php
@@ -1,5 +1,44 @@
 <?php
 
-    
+    session_start();
+
+    require(__DIR__."/fabrico.php");
+
+    // modules + resources
+    global $F;
+    $F->loadModule(
+        "ErrorHandler",
+        "View",
+        "Router", 
+        "DBAdapters/MySQL",
+        "SessionManager",
+        "Former"
+    );
+    $F->loadResource(
+        "config.php",
+        "controllers/*"
+    );
+
+    // configuration of the template engine and Former
+    View::$root = __DIR__."/tpl/";
+    View::$forEachView = array(
+        "siteURL" => ADMINUI_URL
+    );
+    Former::templatesPath(__DIR__."/tpl/Former/");
+
+    // database
+    $mysql = new MySQLAdapter((object) array(
+        "host" => HOST,
+        "user" => USER,
+        "pass" => PASS,
+        "dbname" => DBNAME
+    ));
+
+    // routing
+    $router = new Router();
+    $router
+    ->register("/logout", "Logout")
+    ->register("", array("RequireLogin", "Home"))
+    ->run();
 
 ?>
