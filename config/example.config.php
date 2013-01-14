5df549612a85b5f356a240fe35f8ac94a3a2a2ee
Krasimir Tsonev <krasimir@outset.ws>
2013-01-14 23:14:11 +0200

    Moving config.php

---
+++ config/example.config.php
@@ -0,0 +1,17 @@
+<?php
+
+    define("HOST", "localhost");
+    define("USER", "root");
+    define("PASS", "");
+    define("DBNAME", "krasimir_adminui");
+    define("ADMINUI_URL", "http://admin-ui.dev/");
+    define("FILES_DIR", "files/");
+    define("RESOURCE_DIR", __DIR__."/../resources/")
+
+    global $USERS;
+    $USERS = array(
+        (object) array("username" => "admin", "password" => "admin")
+    );
+    
+
+?>
